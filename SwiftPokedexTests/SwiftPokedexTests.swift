3f10a25959b174fcf4bacee5f1b8d5a766e68a5b
wynant-dev <wynant.dev@gmail.com>
2026-05-19 13:19:46 +0200

    chore: integrate SwiftLint and SwiftFormat

--- SwiftPokedexTests/SwiftPokedexTests.swift
+++ SwiftPokedexTests/SwiftPokedexTests.swift
@@ -5,11 +5,10 @@
 //  Created by Aurélien Wynant on 17.05.26.
 //
 
-import XCTest
 @testable import SwiftPokedex
+import XCTest
 
 final class SwiftPokedexTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -18,19 +17,19 @@ final class SwiftPokedexTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions
+        // afterwards.
     }
 
-    func testPerformanceExample() throws {
+    func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
--- SwiftPokedexUITests/SwiftPokedexUITestsLaunchTests.swift
+++ SwiftPokedexUITests/SwiftPokedexUITestsLaunchTests.swift
@@ -8,7 +8,6 @@
 import XCTest
 
 final class SwiftPokedexUITestsLaunchTests: XCTestCase {
-
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
@@ -18,7 +17,7 @@ final class SwiftPokedexUITestsLaunchTests: XCTestCase {
     }
 
     @MainActor
-    func testLaunch() throws {
+    func testLaunch() {
         let app = XCUIApplication()
         app.launch()
 
